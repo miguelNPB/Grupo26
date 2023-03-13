e272323f03be1e758c919424e98d8fb1f0cdd310
Miguel Pérez <miguelnp@ucm.es>
2023-03-13 11:33:43 +0100

    Archivos iniciales añadidos al repositorio

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MAPmiguel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola soy Miguel");
+        }
+    }
+}
